12cba9023879ac3c85fafc29360ac918538db03b
Abdulazeez <azeezmuritalaadeniyi@gmail.com>
2026-01-12 15:23:16 +0100

    added create user dto

---
+++ ContactManagementSystem/dto/response/CreateUserResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace ContactManagementSyatem.dto.response;
+
+public class CreateUserResponse
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName {get; set; } = null!;
+    public string Email {get; set; } = null!;
+}
